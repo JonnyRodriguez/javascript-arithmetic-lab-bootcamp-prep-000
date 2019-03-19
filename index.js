ec440c718c9ac9ddbfb8e5a67d8aa9502c5c1e88
Jonny Rodriguez <jojoroto@gmail.com>
2019-03-19 21:46:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function subtract(a,b) {
 function multiply(a,b) {
   return a*b;
 }
-divide = (a,b) a/b;
-var inc = n => ++n;
-var dec = n => --n;
+divide = (a,b) => a/b;
+inc = n => ++n;
+dec = n => --n;
